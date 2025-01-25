b115fa0c75e2ff98eb61094c9ededf472955bec4
EyadAbdellatif4 <eyad61.abdellatif61@gmail.com>
2025-01-25 15:25:50 +0200

    end of seciton 6

--- Section 6/controllers/product.controller.js
+++ Section 6/controllers/product.controller.js
@@ -1,4 +1,3 @@
-
 const Product = require("../models/product.model");
 const getProducts = async (req, res) => {
   // Product.find()
@@ -11,10 +10,63 @@ const getProducts = async (req, res) => {
 
   ////// ooooorrrrrr
   try {
-    const products = await Product.find();
+    //// thi is happen because they are looking in the same memory and to solve it we have to use object destructuring
+    // const queryStr = { ...req.query };
+    // console.log(queryStr);
+    // console.log(req.query);
+    // delete queryStr.price;
+    // console.log(queryStr);
+    // console.log(req.query);
+
+    // const queryStr = { ...req.query };
+    // const excludeFields = ["price", "limit"];
+    // excludeFields.forEach((field) => delete queryStr[field]);
+    // console.log(queryStr);
+
+    const queryStr = { ...req.query };
+    let queryObj = JSON.stringify(queryStr);
+    console.log(queryObj);
+    queryObj = queryObj.replace(
+      /\b(gt|gte|lt|lte|ne)\b/g,
+      (match) => `$${match}`
+    );
+    console.log(queryObj);
+
+    // const products = await Product.find(JSON.parse(queryObj));
+
+    let query = Product.find(JSON.parse(queryObj));
+    console.log(re.query);
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      // query = query.sort(req.query.sort);
+      query = query.sort(sortBy);
+    }
+
+    if(req.query.fields){
+        const fields = req.query.fields.split(",").join(" ");
+        query = query.select(fields);
+    }
+
+    if (req.query.page) {
+      const page = req.query.page * 1 || 1;
+      const limit = req.query.limit * 1 || 5;
+      const skip = (page - 1) * limit;
+      query = query.skip(skip).limit(limit);
+    }
+    const productList = await query;
+    // const products = await Product.find(JSON.parse(queryStr)).sort("price");
+    // const products = await Product.find(req.query).sort("-price");
+
+    ///////////////// this will return the query
+    // const products = Product.find(req.query).sort("-price");
+    // console.log(products);
     res
       .status(200)
-      .json({ message: "success", length: products.length, data: products });
+      .json({
+        message: "success",
+        length: productList.length,
+        data: productList,
+      });
   } catch (err) {
     res.status(500).json({ message: "error", data: err });
   }
